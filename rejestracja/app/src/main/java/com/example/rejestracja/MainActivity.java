7f54dd1158a8d3f7f265151339b66b75c53f0e35
GorliG <161640405+GorliG@users.noreply.github.com>
2024-12-05 21:42:19 +0100

    Update MainActivity.java

--- rejestracja/app/src/main/java/com/example/rejestracja/MainActivity.java
+++ rejestracja/app/src/main/java/com/example/rejestracja/MainActivity.java
@@ -1,42 +1,49 @@
 package com.example.rejestracja;
 
 import android.os.Bundle;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Button;
+import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import java.lang.String;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
     private EditText email;
     private EditText haslo;
-    private EditText powhaslo;
+    private EditText powthaslo;
+    private Button zatwierdz;
     private TextView komunikat;
-    private Button wyslij;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-
-        email = findViewById(R.id.poleedycyjneemail);
-        haslo = findViewById(R.id.poleedycyjnehaslo);
-        powhaslo = findViewById(R.id.poleedycyjnepowhaslo);
-        wyslij = findViewById(R.id.zatwierdz);
-        komunikat = findViewById(R.id.komunikaty);
-
-        wyslij.setOnClickListener(v -> {
+        email=findViewById(R.id.editTextEmailAddress);
+        haslo= findViewById(R.id.editTextPassword);
+        powthaslo=findViewById(R.id.editTextPassword2);
+        zatwierdz=findViewById(R.id.button);
+        komunikat=findViewById(R.id.textdokom);
+        
+        zatwierdz.setOnClickListener(view -> {
             String emailText = email.getText().toString().trim();
             String hasloText = haslo.getText().toString().trim();
-            String powhasloText = powhaslo.getText().toString().trim();
+            String powthasloText = powthaslo.getText().toString().trim();
+            if(!emailText.contains("@")){
+                komunikat.setText("Błędny adres e-mail");
 
-            if (!emailText.contains("@")) {
-                komunikat.setText("Błędny adres email!");
-            } else if (!hasloText.equals(powhasloText)) {
-                komunikat.setText("Hasła są różne!");
-            } else {
-                komunikat.setText("Witaj, " + emailText);
+            }else if(!hasloText.equals(powthasloText)){
+                komunikat.setText("Hasła są różne");
+            }else{
+                komunikat.setText("Witaj,"+emailText);
             }
         });
+
+
+
     }
 }
